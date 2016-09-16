04b9c605299a3625fa14740fa87d38d3b12c2cb3
Mikko Uuksulainen <mikko.uuksulainen@gmail.com>
2016-09-16 11:07:53 +0300

    Added NuGet configuration so that latest version can be released to Nuget.org.

--- CodeKicker.BBCode/Properties/AssemblyInfo.cs
+++ CodeKicker.BBCode/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("This is our BBCode-implementation at http://codekicker.de/. It is extremely stable as ist has been tested with Microsoft Pex. Is is also quite performant.")]
 [assembly: AssemblyCompany("http://codekicker.de/")]
 [assembly: AssemblyProduct("CodeKicker.BBCode")]
-[assembly: AssemblyCopyright("In order to use this library, we require a link to http://codekicker.de/ from you. Licensed unter the Creative Commons Attribution 3.0 Licence: http://creativecommons.org/licenses/by/3.0/.")]
+[assembly: AssemblyCopyright("The MIT License (MIT)")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
